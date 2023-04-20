984a6beecb0e25a1aa0b791882192bce6cfaf2fc
joundy <hafizjoundys@gmail.com>
2023-04-20 10:13:20 +0700

    update

--- plugins/core.lua
+++ plugins/core.lua
@@ -1,6 +1,22 @@
 return {
+  -- customize alpha options
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      -- customize the dashboard header
+      opts.section.header.val = {
+        "███╗░░██╗██╗░░░██╗██╗███╗░░░███╗",
+        "████╗░██║██║░░░██║██║████╗░████║",
+        "██╔██╗██║╚██╗░██╔╝██║██╔████╔██║",
+        "██║╚████║░╚████╔╝░██║██║╚██╔╝██║",
+        "██║░╚███║░░╚██╔╝░░██║██║░╚═╝░██║",
+        "╚═╝░░╚══╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
+      }
+      return opts
+    end,
+  },
   -- You can disable default plugins as follows:
-  { "goolord/alpha-nvim", enabled = false },
+  -- { "goolord/alpha-nvim", enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
